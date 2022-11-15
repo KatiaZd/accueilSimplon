f22464913d8a7ffc8c75af9cd7bc63d2c1d542bb
KatiaZd <katia.zouad@hotmail.fr>
2022-11-15 21:39:49 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// MENU BURGER
+
+const burger = document.querySelector(".burger");
+const navMenu = document.querySelector(".navLinks");
+
+burger.addEventListener("click", () => {
+    burger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+
+//CAROUSEL
+
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--soutiens-elements-displayed");
+const marqueeContent = document.querySelector("ul.soutiensContent");
+
+root.style.setProperty("--soutiens-elements", marqueeContent.children.length);
+
+for(let i=0; i<marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
